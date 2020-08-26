c207ec179a60b5347037ec1d3e1e6b6692117f96
lvicen3 <57845715+lvicen3@users.noreply.github.com>
2020-08-26 14:09:28 -0500

    mouse following

--- sketch.js
+++ sketch.js
@@ -1,26 +1,35 @@
 // const { FILL } = require("./p5");
 
 let x = 0;
-let speed = 3;
+let y = 0;
+let x_speed = 3;
+let y_speed = 3;
 
 function setup() {
   // put setup code here
   createCanvas(400,400);
+  background(100,100,100);
 
 }
 
 function draw() {
-  background(100,100,100);
-
+  
   fill(20,30,100);
-  rect(x,20,100,100);
+  if(mouseIsPressed)
+  circle(mouseX,mouseY,10,10);
   // ellipse(120,120,20,100);
   // fill(150);
   
-  if (x>width-100){
-    speed =  -1*speed;x
-  } 
+  // if (x>width-100 || (x < 0 && x_speed <0)){
+  //   x_speed = -1*x_speed;
+  // } 
   
-  x+= speed;
+  // if (y>height-100 || (y < 0 && y_speed <0)){
+  //   y_speed = -1*y_speed;
+  // } 
+
+
+  // x+= x_speed;
+  // y+= y_speed;
 
 }
