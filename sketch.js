b79f82e4d41aab4840a48308a1644a4bc1ed6696
lvicen3 <57845715+lvicen3@users.noreply.github.com>
2020-09-14 19:51:42 -0500

    added gravity and recolored

--- sketch.js
+++ sketch.js
@@ -1,73 +1,29 @@
-// var result = [];
-// var t,c;
-
-// function ease(p){
-//   return 3*p*p-2*p*p*p;
-// }
-
-// function ease(p,g){
-//   if(p<0.5)
-//     return 0.5*Math.pow(2*p,g);
-//   else
-//     return 1-0.5 * pow(2*(1-p),g);
-// }
-
-// var mn = 0.5*sqrt(3), ia = Math.atan(sqrt(.5));
-
-// function push(){
-//   pushMatrix();
-//   pushStyle();
-// }
-
-// function pop(){
-//   popStyle();
-//   popMatrix();
-// }
-
-// function draw(){
-//   if(!recording){
-//     t = mouseX/width;
-//     c = mouseY/height;
-
-//     draw_();
-//   } else {
-//     for(let i = 0; i < widh*height; i++)
-//       for(let a = 0; a < 3; a++)
-//         result[i][a] = 0;
-
-//     c = 0;
-//     for(let sa = 0; sa < samplesPerFrame; sa++){
-//       t = map(frameCount-1+sa*shutterAngle/samplesPerFrame,0,numFrames,0,1);
-//       draw_();
-//       loadPixels();
-//       for(let i = 0; i < pixels.length; i++){
-//         results[i][0] += pixels[i] >> 16 & 0xff;
-//         results[i][1] += pixels[i] >> 8 & 0xff;
-//         results[i][2] += pixels[i] &  0xff;
-        
-//       }
-//     }
-
-//     loadPixels();
-//     for(let i = 0; i < pixels.length;i++)
-//       pixels[i] = 0xff << 24 | 
-//       result[i][0]*1.0/samplesPerFrame << 16 | 
-//       result[i][1]*1.0/samplesPerFrame << 8 | 
-//       result[i][2]*1.0/samplesPerFrame;
-//   }
-// }
-
-/////////////////////////////
+let G = 5;
 
 const particles = [];
-const colors = [['red','rgba(255,0,0,255)'],['green','rgba(0,255,0,255)'],['blue','rgba(0,0,255,255)']];
+const colors2 = [['red','rgba(255,0,0,255)'],['green','rgba(0,255,0,255)'],['blue','rgba(0,0,255,255)']];
+let colors;
+const lines = [];
+
 
 function setup(){
   createCanvas(window.innerWidth, window.innerHeight);
+  background(0);
+
+  colors  = [
+    ['red', color('#ff124f')],
+    ['fuschia', color('#ff0a0')],
+    ['pink', color('#fe75fe')],
+    ['light purple', color('#7a04eb')],
+    ['dark purple', color('#120458')]
+  ];
+
+  for (let i = 0; i < 50; i++) {
+    particles.push(new Particle());
+  }
 }
 
 function draw(){
-    background(0);
 
     particles.forEach((p,index)=>{   
         p.update();
@@ -86,53 +42,83 @@ function mousePressed(){
 
 class Particle{
     constructor(x=-1,y=-1){
-      //Position
+      // Set init position
       if(x < 0 || y < 0){
           this.pos = createVector(random(width),random(height));
       } else {
           this.pos = createVector(x,y);
       }
-      this.size = random(5);
-      //Velocity
-      this.vel = createVector(random(-2,2),random(-2,2));
-      this.z_vel = map(random(),0,1,0.05,0.1);
+
+      // Particle properties
+      this.size = 5;
+      this.mass = random(1,5);
       this.color = random(colors);
+
+      // Movement properties
+      this.vel = createVector(random(-2,2),random(-2,2));
+      this.acc = createVector(0,0);
     }
     
     draw(){
-        noStroke();
-        console.log(this.color[1]);
-        fill(this.color[1]);
-        circle(this.pos.x,this.pos.y,this.size);
+      noStroke();
+      fill(this.color[1]);
+      // circle(this.pos.x,this.pos.y,this.size);
     }
 
     update(){
-        this.pos.add(this.vel);
-        this.size+=this.z_vel;
-        this.edges();
+      this.vel.add(this.acc);
+      this.acc.set(0,0);
+      this.pos.add(this.vel);
+      this.edges();
     }
 
     edges(){
-        if(this.pos.x < 0 || this.pos.x > width){
-            this.vel.x *= -1;
-        }
-        if(this.pos.y < 0 || this.pos.y > height){
-            this.vel.y *= -1;
-        }
-        if(this.size < 0 || this.size > 20)
-            this.z_vel *= -1;
+      if(this.pos.x < 0 || this.pos.x > width){
+          this.vel.x *= -1;
+      }
+      if(this.pos.y < 0 || this.pos.y > height){
+          this.vel.y *= -1;
+      }
     }
 
     checkParticles(particles){
       particles.forEach(particle =>{
         const d = dist(this.pos.x,this.pos.y,particle.pos.x,particle.pos.y);
-
-        if(this.color[0] == particle.color[0]){
+        if(this.color[0] == particle.color[0] && d < 100 && this !== particle){
           stroke(this.color[1]);
           line(this.pos.x,this.pos.y,particle.pos.x,particle.pos.y);
+          this.attract(particle);
         }
         
       });
     }
 
+    // PHYSICS METHODS
+    applyForce(force){
+      let f = p5.Vector.div(force,this.mass);
+      this.acc.add(f);
+    }
+
+    attract(particle){
+      let force = p5.Vector.sub(this.pos,particle.pos);
+      let distanceSq = constrain(force.magSq(),100,1000);
+      let strength = (G * (this.mass*particle.mass))/ distanceSq;
+
+      force.setMag(strength);
+      particle.applyForce(force);
+    }
+}
+
+class Line{
+  constructor(x1,y1,x2,y2,color){
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+    this.color = color;
+  }
+  draw(){
+    stroke(this.color[1]);
+    line(this.x1,this.y1,this.x2,this.y2);
+  }
 }
